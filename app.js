883f3ec9db494ba88ad099d52aaae8cfc6522630
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-21 08:32:31 +0100

    json middleware works

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.status(200).send("<h1>Wagwan</h1>")
 })
 
+app.post("/", (req, res) => {
+    const { name } = req.body;
+    return res.status(200).send(`<h1>${name}</h1>`)
+})
+
 
 
 app.listen(3000, () => {
