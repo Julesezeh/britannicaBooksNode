1ec9d9db9a1260f8148d862104cf47c5ddcf0564
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-21 08:27:28 +0100

    first request

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Wagwan</h1>")
+})
+
+
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000")
